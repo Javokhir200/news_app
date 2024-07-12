c45b3faac50368bc0be0e1458fd5c1f581450242
javokhir200 <javokhirganiboyev2004@gmail.com>
2024-07-12 18:38:41 +0500

    firstt launch

---
+++ src/test/java/uz/lee/news_app/NewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.lee.news_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
