47a072698dff4142899771e30dd03c347bebb5df
stranger <ismoiljonovabdulaziz97@gmail.com>
2024-07-24 14:30:45 +0500

    Everything needed added, Please check first and Merge

---
+++ src/main/java/uz/lee/news_app/feign/APIFeign.java
@@ -0,0 +1,11 @@
+package uz.lee.news_app.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import uz.lee.news_app.feignDtos.APIResponseDTO;
+
+@FeignClient(name = "apiClient", url = "http://api.com/api")
+public interface APIFeign {
+    @GetMapping()
+    APIResponseDTO getAPIData();
+}
---
+++ src/main/java/uz/lee/news_app/feign/WeatherClient.java
@@ -0,0 +1,17 @@
+package uz.lee.news_app.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import uz.lee.news_app.feignDtos.WeatherResponseDTO;
+
+@FeignClient(name = "openWeatherMapClient", url = "https://api.openweathermap.org/data/2.5")
+public interface WeatherClient {
+    @GetMapping("/onecall")
+    WeatherResponseDTO get7DayWeather(
+        @RequestParam("lat") double latitude,
+        @RequestParam("lon") double longitude,
+        @RequestParam("exclude") String exclude,
+        @RequestParam("appid") String apiKey
+    );
+}
